7295440578deb9bd422a04cdf83672cd90bfafda
garcialucasm <116840436+garcialucasm@users.noreply.github.com>
2023-09-16 20:01:35 +0100

    adding js projects about nodejs and npm

---
+++ node.js/npm/index.js
@@ -0,0 +1,12 @@
+//Importing and using npm
+import generateStupidName from "sillyname";
+
+var sillyName = generateStupidName();
+
+console.log(`The name is  ${sillyName}`);
+
+// import superheroes from 'superheroes';
+
+// const randomName = superheroes.random();
+
+// console.log(`The superhero is ${randomName}!`);
